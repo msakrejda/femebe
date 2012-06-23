c10cd4e32e6b674280edffc960370aace6e4f924
Dan Farina <drfarina@acm.org>
2012-06-22 17:21:53 -0700

    Make buffer type reader/writer more generic
    This allows direct pipelining of the written output to bufio or
    sockets or whatever.
    
    Signed-off-by: Dan Farina <drfarina@acm.org>

--- buffer.go
+++ buffer.go
@@ -1,53 +1,59 @@
 package femebe
 
 import (
-	"bytes"
 	"encoding/binary"
+	"io"
 )
 
-func WriteInt16(b *bytes.Buffer, val int16) {
+// ReadBytes() is defined in a couple of go packages (bufio, bytes)
+// but not reified into an interface, so do that here.
+type ReadByteser interface {
+	ReadBytes(delim byte) (line []byte, err error)
+}
+
+func WriteInt16(w io.Writer, val int16) {
 	valBytes := make([]byte, 2)
 	binary.BigEndian.PutUint16(valBytes, uint16(val))
-	b.Write(valBytes)
+	w.Write(valBytes)
 }
 
-func WriteInt32(b *bytes.Buffer, val int32) {
+func WriteInt32(w io.Writer, val int32) {
 	valBytes := make([]byte, 4)
 	binary.BigEndian.PutUint32(valBytes, uint32(val))
-	b.Write(valBytes)
+	w.Write(valBytes)
 }
 
-func WriteCString(b *bytes.Buffer, val string) {
-	b.WriteString(val)
-	b.WriteByte('\000')
+func WriteCString(w io.Writer, val string) {
+	io.WriteString(w, val)
+	w.Write([]byte{'\000'})
 }
 
-func ReadInt16(b *bytes.Buffer) int16 {
+func ReadInt16(r io.Reader) int16 {
 	valBytes := make([]byte, 2)
-	b.Read(valBytes)
+	r.Read(valBytes)
 	return int16(binary.BigEndian.Uint16(valBytes))
 }
 
-func ReadUInt16(b *bytes.Buffer) uint16 {
+func ReadUInt16(r io.Reader) uint16 {
 	valBytes := make([]byte, 2)
-	b.Read(valBytes)
+	r.Read(valBytes)
 	return uint16(binary.BigEndian.Uint16(valBytes))
 }
 
-func ReadInt32(b *bytes.Buffer) int32 {
+func ReadInt32(r io.Reader) int32 {
 	valBytes := make([]byte, 4)
-	b.Read(valBytes)
+	r.Read(valBytes)
 	return int32(binary.BigEndian.Uint32(valBytes))
 }
 
-func ReadUInt32(b *bytes.Buffer) uint32 {
+func ReadUInt32(r io.Reader) uint32 {
 	valBytes := make([]byte, 4)
-	b.Read(valBytes)
+	r.Read(valBytes)
 	return uint32(binary.BigEndian.Uint32(valBytes))
 }
 
-func ReadCString(b *bytes.Buffer) string {
-	line, _ := b.ReadBytes('\000')
+func ReadCString(r ReadByteser) string {
+	line, _ := r.ReadBytes('\000')
 	lineLen := len(line)
 	return string(line[0 : lineLen-1])
 }
