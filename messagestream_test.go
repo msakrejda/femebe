8ce8ec7fad7a58647ceb329f6d628970ce81e085
Dan Farina <drfarina@acm.org>
2012-07-09 18:42:15 -0700

    Add testing of promise messages
    Intended to exercise when messages are fragmented across a buffered
    boundary.
    
    Signed-off-by: Dan Farina <drfarina@acm.org>

--- messagestream_test.go
+++ messagestream_test.go
@@ -2,10 +2,11 @@ package femebe
 
 import (
 	"bytes"
+	"io"
 	"testing"
 )
 
-func TestFullyBuffered(t *testing.T) {
+func newTestMessageStream(t *testing.T) *MessageStream {
 	buf := bytes.NewBuffer(make([]byte, 0, 1024))
 	ms := MessageStream{
 		Name:         "Test",
@@ -14,6 +15,11 @@ func TestFullyBuffered(t *testing.T) {
 		be:           &binEnc{},
 	}
 
+	return &ms
+}
+
+func TestFullyBuffered(t *testing.T) {
+	ms := newTestMessageStream(t)
 	var m Message
 
 	InitAuthenticationOk(&m)
@@ -36,6 +42,73 @@ func TestFullyBuffered(t *testing.T) {
 
 	// The very last HasNext call must return false
 	if ms.HasNext() {
+		t.Fatal()
+	}
+}
+
+func TestPromise(t *testing.T) {
+	// Write a complete AuthenticationOk message to a buffer.
+	var m Message
+	InitAuthenticationOk(&m)
+
+	buf := bytes.NewBuffer(make([]byte, 0, 1024))
+	m.WriteTo(buf)
+
+	// Slice it apart: the five-byte prefix is a minimal message
+	// header.
+	header := make([]byte, 5)
+	buf.Read(header)
+
+	// Conjure a MessageStream where only those five bytes have
+	// been received from the network.
+	ms := newTestMessageStream(t)
+	ms.msgRemainder.Write(header)
+
+	// 'buf' is in the right spot to form the rest of the
+	// Promise-style message.
+	ms.r = buf
+
+	// 5 bytes is enough to have a next message.
+	if !ms.HasNext() {
+		t.Fatal()
+	}
+
+	// Grab the message and check the contents of the payload.
+	ms.Next(&m)
+
+	checkBuf := bytes.NewBuffer(make([]byte, 0, 1024))
+	io.Copy(checkBuf, m.Payload())
+
+	if !bytes.Equal(checkBuf.Bytes(), []byte{0x0, 0x0, 0x0, 0x0}) {
+		t.Fatal()
+	}
+}
+
+func TestIncompleteMessage(t *testing.T) {
+	var m Message
+	InitAuthenticationOk(&m)
+
+	buf := bytes.NewBuffer(make([]byte, 0, 1024))
+	m.WriteTo(buf)
+
+	header := make([]byte, 5)
+	buf.Read(header)
+
+	ms := newTestMessageStream(t)
+	ms.msgRemainder.Write(header)
+
+	ms.r = buf
+
+	if !ms.HasNext() {
+		t.Fatal()
+	}
+
+	ms.Next(&m)
+
+	checkBuf := bytes.NewBuffer(make([]byte, 0, 1024))
+	io.Copy(checkBuf, m.Payload())
+
+	if !bytes.Equal(checkBuf.Bytes(), []byte{0x0, 0x0, 0x0, 0x0}) {
 		t.Fatal()
 	}
 }
